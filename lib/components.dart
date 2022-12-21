87d0fffdcde26e8d5ae5b8fcf135085ea7eb6666
Paulina Knop <paulinaeknop@gmail.com>
2022-12-21 23:12:40 +0530

    Reusing code for Login View.

--- lib/components.dart
+++ lib/components.dart
@@ -1,6 +1,10 @@
+import 'package:budget_app_starting/view_model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sign_button/sign_button.dart';
 
 class Pacifico extends StatelessWidget {
   final text;
@@ -238,6 +242,154 @@ class TextForm extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+TextEditingController _emailField = TextEditingController();
+
+TextEditingController _passwordField = TextEditingController();
+
+class EmailAndPasswordFields extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModelProvider = ref.watch(viewModel);
+    return Column(
+      children: [
+        //Email
+        SizedBox(
+          width: 350.0,
+          child: TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textAlign: TextAlign.center,
+            controller: _emailField,
+            decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Colors.black,
+                  size: 30.0,
+                ),
+                hintText: "Email",
+                hintStyle: GoogleFonts.openSans()),
+          ),
+        ),
+        SizedBox(height: 20),
+        //Password
+        SizedBox(
+          width: 350.0,
+          child: TextFormField(
+            textAlign: TextAlign.center,
+            controller: _passwordField,
+            obscureText: viewModelProvider.isObscure,
+            decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                prefixIcon: IconButton(
+                  icon: Icon(
+                    viewModelProvider.isObscure
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    color: Colors.black,
+                    size: 30.0,
+                  ),
+                  onPressed: () {
+                    viewModelProvider.toggleObscure();
+                  },
+                ),
+                hintText: "Password",
+                hintStyle: GoogleFonts.openSans()),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RegisterAndLogin extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModelProvider = ref.watch(viewModel);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 50.0,
+          width: 150.0,
+          child: MaterialButton(
+            child: OpenSans(
+              text: "Register",
+              size: 25.0,
+              color: Colors.white,
+            ),
+            onPressed: () async {
+              await viewModelProvider.createUserWithEmailAndPassword(
+                  context, _emailField.text, _passwordField.text);
+            },
+            splashColor: Colors.grey,
+            color: Colors.black,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+          ),
+        ),
+
+        SizedBox(
+          width: 20.0,
+        ),
+        Pacifico(
+          text: "OR",
+          size: 15.0,
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        //Login
+        SizedBox(
+          height: 50.0,
+          width: 150.0,
+          child: MaterialButton(
+            child: OpenSans(
+              text: "Login",
+              size: 25.0,
+              color: Colors.white,
+            ),
+            onPressed: () async {
+              await viewModelProvider.signInWithEmailAndPassword(
+                  context, _emailField.text, _passwordField.text);
+            },
+            splashColor: Colors.grey,
+            color: Colors.black,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class GoogleSignInButton extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModelProvider = ref.watch(viewModel);
+    return //Google signIn
+        SignInButton(
+      buttonType: ButtonType.google,
+      btnColor: Colors.black,
+      btnTextColor: Colors.white,
+      buttonSize: ButtonSize.large, // small(default), medium, large
+      onPressed: () async {
+        if (kIsWeb) {
+          await viewModelProvider.signInWithGoogleWeb(context);
+        } else {
+          await viewModelProvider.signInWithGoogleMobile(context);
+        }
+      },
     );
   }
 }
--- lib/mobile/login_view_mobile.dart
+++ lib/mobile/login_view_mobile.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sign_button/sign_button.dart';
 
 import '../components.dart';
 import '../view_model.dart';
@@ -11,8 +7,6 @@ import '../view_model.dart';
 class LoginViewMobile extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    TextEditingController _emailField = useTextEditingController();
-    TextEditingController _passwordField = useTextEditingController();
     final viewModelProvider = ref.watch(viewModel);
     final double deviceHeight = MediaQuery.of(context).size.height;
     //  final double categoryWidth = MediaQuery.of(context).size.width;
@@ -32,136 +26,17 @@ class LoginViewMobile extends HookConsumerWidget {
               SizedBox(
                 height: 30.0,
               ),
-              //Email address
-              SizedBox(
-                width: 350.0,
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  textAlign: TextAlign.center,
-                  controller: _emailField,
-                  decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Colors.black,
-                        size: 30.0,
-                      ),
-                      hintText: "Email",
-                      hintStyle: GoogleFonts.openSans()),
-                ),
-              ),
-              SizedBox(height: 20),
-              //Password
-              SizedBox(
-                width: 350.0,
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: _passwordField,
-                  obscureText: viewModelProvider.isObscure,
-                  decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      prefixIcon: IconButton(
-                        icon: Icon(
-                          viewModelProvider.isObscure
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          color: Colors.black,
-                          size: 30.0,
-                        ),
-                        onPressed: () {
-                          viewModelProvider.toggleObscure();
-                        },
-                      ),
-                      hintText: "Password",
-                      hintStyle: GoogleFonts.openSans()),
-                ),
-              ),
+              EmailAndPasswordFields(),
               SizedBox(
                 height: 30.0,
               ),
               //Register
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 50.0,
-                    width: 150.0,
-                    child: MaterialButton(
-                      child: OpenSans(
-                        text: "Register",
-                        size: 25.0,
-                        color: Colors.white,
-                      ),
-                      onPressed: () async {
-                        await viewModelProvider.createUserWithEmailAndPassword(
-                            context, _emailField.text, _passwordField.text);
-                      },
-                      splashColor: Colors.grey,
-                      color: Colors.black,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0)),
-                    ),
-                  ),
-
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  Pacifico(
-                    text: "OR",
-                    size: 15.0,
-                  ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  //Login
-                  SizedBox(
-                    height: 50.0,
-                    width: 150.0,
-                    child: MaterialButton(
-                      child: OpenSans(
-                        text: "Login",
-                        size: 25.0,
-                        color: Colors.white,
-                      ),
-                      onPressed: () async {
-                        await viewModelProvider.signInWithEmailAndPassword(
-                            context, _emailField.text, _passwordField.text);
-                      },
-                      splashColor: Colors.grey,
-                      color: Colors.black,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0)),
-                    ),
-                  ),
-                ],
-              ),
+              RegisterAndLogin(),
               SizedBox(
                 height: 30,
               ),
               //Google signIn
-              SignInButton(
-                buttonType: ButtonType.google,
-                btnColor: Colors.black,
-                btnTextColor: Colors.white,
-                buttonSize: ButtonSize.medium, // small(default), medium, large
-                onPressed: () async {
-                  if (kIsWeb) {
-                    await viewModelProvider.signInWithGoogleWeb(context);
-                  } else {
-                    await viewModelProvider.signInWithGoogleMobile(context);
-                  }
-                },
-              ),
+              GoogleSignInButton()
             ],
           ),
         ),
